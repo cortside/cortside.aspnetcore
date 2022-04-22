075ff004eedc231b3994f796c66485d335eacd51
Cort Schaefer <cort@eschaefer.net>
2022-04-21 22:10:57 -0600

    add more extensions for common Startup configuration

--- src/Cortside.AspNetCore.Swagger/ServiceCollectionExtensions.cs
+++ src/Cortside.AspNetCore.Swagger/ServiceCollectionExtensions.cs
@@ -32,7 +32,7 @@ namespace Cortside.AspNetCore.Swagger {
                 }
 
                 // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
 
