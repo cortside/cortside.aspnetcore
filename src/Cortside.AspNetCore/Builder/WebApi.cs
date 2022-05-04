76df09dc60480676d93b1ae253b80a6589d57e86
Cort Schaefer <cort@eschaefer.net>
2022-05-04 00:07:06 -0600

    refactor builder

--- src/Cortside.AspNetCore/Builder/WebApi.cs
+++ src/Cortside.AspNetCore/Builder/WebApi.cs
@@ -1,18 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
-using Cortside.Bowdlerizer;
-using Cortside.Health.Models;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using Serilog.Bowdlerizer;
 
 namespace Cortside.AspNetCore.Builder {
     /// <summary>
@@ -40,94 +29,29 @@ namespace Cortside.AspNetCore.Builder {
         /// <param name="args">The arguments.</param>
         /// <param name="config">The configuration.</param>
         public async Task<int> StartAsync() {
-            var build = builder.Configuration.GetSection("Build").Get<BuildModel>();
-            var service = Assembly.GetEntryAssembly().GetName();
-            var executingIsEntryAssembly = ExecutingIsEntryAssembly();
-
-            var rules = builder.Configuration.GetSection("Bowdlerizer").Get<List<BowdlerizerRuleConfiguration>>();
-            var bowdlerizer = new Bowdlerizer.Bowdlerizer(rules);
-
-            var loggerConfiguration = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Configuration)
-                .Enrich.FromLogContext()
-                .Enrich.WithProperty("Environment", Environment)
-                .Enrich.WithProperty("Service", service)
-                .Enrich.WithProperty("BuildVersion", build.Version)
-                .Enrich.WithProperty("BuildTag", build.Tag)
-                .Enrich.WithBowdlerizer(bowdlerizer);
-
-            var serverUrl = builder.Configuration["Seq:ServerUrl"];
-            if (!string.IsNullOrWhiteSpace(serverUrl)) {
-                loggerConfiguration.WriteTo.Seq(serverUrl);
-            }
-            var logFile = builder.Configuration["LogFile:Path"];
-            if (!string.IsNullOrWhiteSpace(logFile)) {
-                loggerConfiguration.WriteTo.File(logFile);
-            }
-            Log.Logger = loggerConfiguration.CreateLogger();
-
             CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 
             try {
                 // don't log these when the entry assembly is not _this_ assembly, meaning that it's
                 // being executed by something like dotnet ef for migrations
-                if (executingIsEntryAssembly) {
-                    Log.Information("Starting {Service}", service);
-                    Log.Information("ASPNETCORE environment = {Environment}", Environment);
+                if (builder.ExecutingIsEntryAssembly) {
+                    Log.Information("Starting {Service}", builder.Service);
+                    Log.Information("ASPNETCORE environment = {Environment}", WebApiBuilder.Environment);
                 }
-                var appBuilder = WebApplication.CreateBuilder(builder.Args);
-                appBuilder.Host.UseSerilog(Log.Logger);
 
-                appBuilder.WebHost.ConfigureAppConfiguration(b => b.AddConfiguration(builder.Configuration));
-                appBuilder.WebHost.UseConfiguration(builder.Configuration);
-                appBuilder.WebHost.UseShutdownTimeout(TimeSpan.FromSeconds(10));
-                appBuilder.WebHost.ConfigureKestrel(options => {
-                    options.AddServerHeader = false;
-                    options.Limits.MaxRequestLineSize = int.MaxValue;
-                    options.Limits.MaxRequestBufferSize = int.MaxValue;
-                });
-                appBuilder.WebHost.UseDefaultServiceProvider(options => options.ValidateScopes = false);
-
-                appBuilder.Services.AddSingleton(bowdlerizer);
-
-                builder.Startup.ConfigureServices(appBuilder.Services);
-
-                var app = appBuilder.Build();
-
-                var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-                builder.Startup.Configure(app, app.Environment, provider);
+                var app = builder.WebApplication;
 
                 // start host along with any startup tasks
                 await app.RunWithTasksAsync().ConfigureAwait(false);
 
                 return 0;
-            } catch (Exception ex) when (ex is not OperationCanceledException && ex.GetType().Name != "StopTheHostException" && !executingIsEntryAssembly) {
+            } catch (Exception ex) when (ex is not OperationCanceledException && ex.GetType().Name != "StopTheHostException" && !builder.ExecutingIsEntryAssembly) {
                 // FYI the exception filter is to handle dotnet ef commands when dealing with migrations
                 Log.Fatal(ex, "Host terminated unexpectedly");
                 return 1;
             } finally {
                 Log.CloseAndFlush();
             }
-        }
-
-        private static bool ExecutingIsEntryAssembly() {
-            var entry = Assembly.GetEntryAssembly();
-            var calling = Assembly.GetCallingAssembly();
-            var executing = Assembly.GetExecutingAssembly();
-
-            return calling == entry && entry == executing;
-        }
-
-        public static string Environment => System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
-
-        public static IConfiguration GetConfiguration() {
-            return new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
-                .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("build.json", true, true)
-                .AddEnvironmentVariables()
-                .Build();
         }
     }
 }
--- src/Cortside.AspNetCore/Builder/WebApiBuilder.cs
+++ src/Cortside.AspNetCore/Builder/WebApiBuilder.cs
@@ -1,14 +1,28 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using Cortside.Bowdlerizer;
+using Cortside.Health.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Bowdlerizer;
 
 namespace Cortside.AspNetCore.Builder {
     public class WebApiBuilder {
         private IWebApiStartup startup;
         private IConfiguration config;
         private string[] args;
+        private WebApplication webApplication;
+        private Bowdlerizer.Bowdlerizer bowdlerizer;
+        private LoggerConfiguration loggerConfiguration;
+        private string service;
+        private bool executingIsEntryAssembly;
 
         public WebApiBuilder(string[] args) {
             this.args = args;
@@ -29,6 +43,38 @@ namespace Cortside.AspNetCore.Builder {
         public IWebApiStartup Startup => startup;
         public IConfiguration Configuration => config;
         public string[] Args => args;
+
+        public bool ExecutingIsEntryAssembly => executingIsEntryAssembly;
+
+        public string Service => service;
+
+        public WebApplication WebApplication => webApplication;
+
+        private void CreateWebApplication() {
+            var appBuilder = WebApplication.CreateBuilder(Args);
+            appBuilder.Host.UseSerilog(Log.Logger);
+
+            appBuilder.WebHost.ConfigureAppConfiguration(b => b.AddConfiguration(Configuration));
+            appBuilder.WebHost.UseConfiguration(Configuration);
+            appBuilder.WebHost.UseShutdownTimeout(TimeSpan.FromSeconds(10));
+            appBuilder.WebHost.ConfigureKestrel(options => {
+                options.AddServerHeader = false;
+                options.Limits.MaxRequestLineSize = int.MaxValue;
+                options.Limits.MaxRequestBufferSize = int.MaxValue;
+            });
+            appBuilder.WebHost.UseDefaultServiceProvider(options => options.ValidateScopes = false);
+
+            appBuilder.Services.AddSingleton(bowdlerizer);
+
+            Startup.ConfigureServices(appBuilder.Services);
+
+            var app = appBuilder.Build();
+
+            var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+            Startup.Configure(app, app.Environment, provider);
+
+            this.webApplication = app;
+        }
 
         public IServiceCollection Services { get; }
         public ILoggingBuilder Logging { get; }
@@ -51,10 +97,54 @@ namespace Cortside.AspNetCore.Builder {
         }
 
         public WebApi Build() {
+            config ??= GetConfiguration();
+
             if (startup != null) {
                 startup.UseConfiguration(config);
             }
+
+            var build = config.GetSection("Build").Get<BuildModel>();
+            service = Assembly.GetEntryAssembly().GetName().FullName;
+            executingIsEntryAssembly = GetExecutingIsEntryAssembly();
+
+            var rules = config.GetSection("Bowdlerizer").Get<List<BowdlerizerRuleConfiguration>>();
+            bowdlerizer = new Bowdlerizer.Bowdlerizer(rules);
+
+            loggerConfiguration ??= GetLoggerConfiguration(build, service, bowdlerizer);
+            Log.Logger = loggerConfiguration.CreateLogger();
+
+            CreateWebApplication();
             return new WebApi(this);
+        }
+
+        private static bool GetExecutingIsEntryAssembly() {
+            var entry = Assembly.GetEntryAssembly();
+            var calling = Assembly.GetCallingAssembly();
+            var executing = Assembly.GetExecutingAssembly();
+
+            return calling == entry && entry == executing;
+        }
+
+        private LoggerConfiguration GetLoggerConfiguration(BuildModel build, string service, Bowdlerizer.Bowdlerizer bowdlerizer) {
+            var loggerConfiguration = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Environment", Environment)
+                .Enrich.WithProperty("Service", service)
+                .Enrich.WithProperty("BuildVersion", build.Version)
+                .Enrich.WithProperty("BuildTag", build.Tag)
+                .Enrich.WithBowdlerizer(bowdlerizer);
+
+            var serverUrl = config["Seq:ServerUrl"];
+            if (!string.IsNullOrWhiteSpace(serverUrl)) {
+                loggerConfiguration.WriteTo.Seq(serverUrl);
+            }
+            var logFile = config["LogFile:Path"];
+            if (!string.IsNullOrWhiteSpace(logFile)) {
+                loggerConfiguration.WriteTo.File(logFile);
+            }
+
+            return loggerConfiguration;
         }
     }
 }
