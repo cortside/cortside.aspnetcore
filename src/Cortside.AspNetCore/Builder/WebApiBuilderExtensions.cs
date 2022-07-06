56d8e439b9740ab2f9c1a573f86a2c20f05e0e93
Cort Schaefer <cort@eschaefer.net>
2022-07-05 21:27:18 -0600

    [ValidationListException] add extension methods for configuring webapi builder

---
+++ src/Cortside.AspNetCore/Builder/WebApiBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cortside.AspNetCore.Builder {
+    public static class WebApiBuilderExtensions {
+        public static WebApiBuilder ConfigureServices(this WebApiBuilder builder, Action<IServiceCollection> action) {
+            action(builder.Services);
+            return builder;
+        }
+    }
+}
