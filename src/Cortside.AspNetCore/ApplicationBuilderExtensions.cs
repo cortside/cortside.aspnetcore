8d2b1eb1e8abc9eaf4f462bee02248a846a5f47e
Cort Schaefer <cort@eschaefer.net>
2022-07-29 09:13:59 -0600

    add exception handler to complete task so that correlationId still happens in response headers

--- src/Cortside.AspNetCore/ApplicationBuilderExtensions.cs
+++ src/Cortside.AspNetCore/ApplicationBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Cortside.Common.Correlation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
@@ -7,6 +8,7 @@ namespace Cortside.AspNetCore {
     public static class ApplicationBuilderExtensions {
         public static IApplicationBuilder UseApiDefaults(this IApplicationBuilder app, IConfiguration config) {
             app.UseMiddleware<CorrelationMiddleware>();
+            app.UseExceptionHandler(error => error.Run(_ => Task.CompletedTask));
             app.UseResponseCompression();
             app.UseResponseCaching();
             app.UseCors(builder => builder
