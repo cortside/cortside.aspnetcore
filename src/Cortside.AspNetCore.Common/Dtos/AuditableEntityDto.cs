2e714b104732075d71b54aeef508cf726774dca6
Cort Schaefer <cort@eschaefer.net>
2023-02-16 15:10:22 -0700

    add dto classes to go with auditable entity classes

---
+++ src/Cortside.AspNetCore.Common/Dtos/AuditableEntityDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cortside.AspNetCore.Common.Dtos {
+    public class AuditableEntityDto {
+        public DateTime CreatedDate { get; set; }
+
+        public SubjectDto CreatedSubject { get; set; }
+
+        public DateTime LastModifiedDate { get; set; }
+
+        public SubjectDto LastModifiedSubject { get; set; }
+    }
+}
---
+++ src/Cortside.AspNetCore.Common/Dtos/SubjectDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cortside.AspNetCore.Common.Dtos {
+    public class SubjectDto {
+        public Guid SubjectId { get; set; }
+
+        public string Name { get; set; }
+
+        public string GivenName { get; set; }
+
+        public string FamilyName { get; set; }
+
+        public string UserPrincipalName { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
