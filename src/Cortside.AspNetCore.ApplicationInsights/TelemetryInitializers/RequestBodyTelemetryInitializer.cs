10477b1baa00659567a01bb0efbe40e53f19d078
Cort Schaefer <cort@eschaefer.net>
2023-04-11 13:29:36 -0600

    [ARC-120] add new logging middleware and app insights telementy initializer for logging request ip address

---
+++ src/Cortside.AspNetCore.ApplicationInsights/TelemetryInitializers/RequestBodyTelemetryInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+
+namespace Cortside.AspNetCore.ApplicationInsights.TelemetryInitializers {
+    public class RequestBodyTelemetryInitializer : ITelemetryInitializer {
+        private const string PROPERTY_KEY = "RequestBody";
+        readonly IHttpContextAccessor httpContextAccessor;
+
+        public RequestBodyTelemetryInitializer(IHttpContextAccessor httpContextAccessor) {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Initialize(ITelemetry telemetry) {
+            if (telemetry is RequestTelemetry requestTelemetry) {
+                if ((httpContextAccessor.HttpContext.Request.Method == HttpMethods.Post ||
+                        httpContextAccessor.HttpContext.Request.Method == HttpMethods.Put) &&
+                        httpContextAccessor.HttpContext.Request.Body.CanRead) {
+
+                    if (requestTelemetry.Properties.ContainsKey(PROPERTY_KEY)) {
+                        return;
+                    }
+
+                    httpContextAccessor.HttpContext.Request.EnableRewind();
+                    var sr = new StreamReader(httpContextAccessor.HttpContext.Request.Body);
+                    var bodyContent = sr.ReadToEnd();
+                    httpContextAccessor.HttpContext.Request.Body.Position = 0;
+                    requestTelemetry.Properties.Add(PROPERTY_KEY, bodyContent);
+                }
+            }
+        }
+    }
+}
---
+++ src/Cortside.AspNetCore/Middleware/RequestIpAddressLoggingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cortside.AspNetCore.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Cortside.AspNetCore.Auditable.Middleware {
+    public class RequestIpAddressLoggingMiddleware {
+        private const string PROPERTY_KEY = "RequestIpAddress";
+        private readonly ILogger<RequestIpAddressLoggingMiddleware> logger;
+        private readonly RequestDelegate next;
+
+        public RequestIpAddressLoggingMiddleware(ILogger<RequestIpAddressLoggingMiddleware> logger, RequestDelegate next) {
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context) {
+            var properties = new Dictionary<string, object> {
+                [PROPERTY_KEY] = HttpContextUtility.GetRequestIpAddress(context)
+            };
+
+            using (logger.BeginScope(properties)) {
+                await next(context);
+            }
+        }
+    }
+}
