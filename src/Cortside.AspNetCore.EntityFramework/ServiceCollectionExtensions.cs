984ea1833a3c2950b6ff77f101971ee219da9736
Cort Schaefer <cort@eschaefer.net>
2023-05-11 17:24:41 -0600

    [ARC-136] add service colleciton extention method to register database context

---
+++ src/Cortside.AspNetCore.EntityFramework/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Cortside.AspNetCore.EntityFramework.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cortside.AspNetCore.EntityFramework {
+    public static class ServiceCollectionExtensions {
+        /// <summary>
+        /// Registers sql server database context with connection string from Database:ConnectionString in configuration
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDatabaseContext<TInterface, TImplementation>(this IServiceCollection services, IConfigurationRoot configuration)
+                where TImplementation : DbContext, TInterface, IUnitOfWork
+                where TInterface : class {
+            services.AddDbContext<TImplementation>(opt => {
+                opt.UseSqlServer(configuration.GetSection("Database").GetValue<string>("ConnectionString"),
+                    sqlServerOptionsAction: sqlOptions => {
+                        // can not use EnableRetryOnFailure because of the use of user initiated transactions
+
+                        // instruct ef to use multiple queries instead of large joined queries
+                        sqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    });
+                opt.EnableServiceProviderCaching();
+                opt.AddInterceptors(new QueryHintCommandInterceptor());
+            });
+
+            // register the dbcontext interface that is to be used
+            services.AddScoped<TInterface>(provider => provider.GetService<TImplementation>());
+
+            // Register the service and implementation for the database context
+            services.AddScoped<IUnitOfWork>(provider => provider.GetService<TImplementation>());
+
+            // for DbContextCheck in cortside.health
+            services.AddTransient<DbContext, TImplementation>();
+
+            return services;
+        }
+    }
+}
