e65bd6a573cd2d37d16daf1386bd9125832f398b
Cort Schaefer <cort@eschaefer.net>
2023-05-24 12:22:01 -0600

    add additional extention methods to make registrations easier

--- src/Cortside.AspNetCore.Swagger/ServiceCollectionExtensions.cs
+++ src/Cortside.AspNetCore.Swagger/ServiceCollectionExtensions.cs
@@ -23,7 +23,7 @@ namespace Cortside.AspNetCore.Swagger {
                 });
             }
 
-            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlFile = $"{Assembly.GetEntryAssembly()?.GetName().Name}.xml";
             services.AddSwagger(configuration, xmlFile, apiversions);
 
             return services;
