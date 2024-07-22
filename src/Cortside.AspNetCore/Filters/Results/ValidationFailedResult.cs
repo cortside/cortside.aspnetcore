b730e8a13eeffa25cc35ddb4d6b2d972a8693e07
Braden Edmunds <bedmunds@tula-health.com>
2024-07-22 15:16:13 -0600

    remove this unused code

--- src/Cortside.AspNetCore/Filters/Results/ValidationFailedResult.cs
+++ src/Cortside.AspNetCore/Filters/Results/ValidationFailedResult.cs
@@ -6,9 +6,6 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 namespace Cortside.AspNetCore.Filters.Results {
     public class ValidationFailedResult : ModelStateResult {
         public ValidationFailedResult(ModelStateDictionary modelState) : base(modelState) {
-            var errors = modelState.Keys
-                        .SelectMany(key => modelState[key].Errors.Select(x => new ErrorModel(x.Exception?.GetType()?.Name ?? "ModelStateValidation", key, x.ErrorMessage)))
-                        .ToList();
             StatusCode = StatusCodes.Status400BadRequest;
         }
     }
