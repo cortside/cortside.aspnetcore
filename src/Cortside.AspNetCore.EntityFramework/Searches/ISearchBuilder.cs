b0e2458df1887c5977d1650786fca89bee27aeea
Cort Schaefer <cort@eschaefer.net>
2024-07-31 16:20:53 -0600

    rename parameter to match shoppingcart-api

--- src/Cortside.AspNetCore.EntityFramework/Searches/ISearchBuilder.cs
+++ src/Cortside.AspNetCore.EntityFramework/Searches/ISearchBuilder.cs
@@ -2,6 +2,6 @@ using System.Linq;
 
 namespace Cortside.AspNetCore.EntityFramework.Searches {
     public interface ISearchBuilder<T> {
-        IQueryable<T> Build(IQueryable<T> list);
+        IQueryable<T> Build(IQueryable<T> entities);
     }
 }
