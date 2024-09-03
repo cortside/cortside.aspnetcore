b778605c5e42789f1834d2f6a34514099627c60a
Cort Schaefer <cort@eschaefer.net>
2024-09-02 18:23:46 -0600

    update to current version of aspnet versioning

--- src/Cortside.AspNetCore/Builder/WebApiHostExtensions.cs
+++ src/Cortside.AspNetCore/Builder/WebApiHostExtensions.cs
@@ -1,7 +1,7 @@
 using System;
+using Asp.Versioning.ApiExplorer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cortside.AspNetCore.Builder {
